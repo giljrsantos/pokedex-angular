0e6cb1b8a6935ce5eacaef1deb508de09bf3aab9
Gilberto Jr <giljrsantos@gmail.com>
2023-01-31 22:12:20 -0300

    Primeiro Commit Criando Estrutura Projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+//Modules
+import { PagesModule } from './pages/pages.module';
+
+//Components
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+// Pages
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
