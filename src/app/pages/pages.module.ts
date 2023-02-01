ca9d66da54ada323d68d6b73231b4c338a2b6e6f
Gilberto Jr <giljrsantos@gmail.com>
2023-01-31 22:52:26 -0300

    Declarando DetailsComponent e Importando RoutingModule

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,13 +2,18 @@ import { CommonModule } from '@angular/common';
 // Pages
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
+// Modele Routing
+import { RoutingModule } from './routing-module';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    DetailsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RoutingModule
   ]
 })
 export class PagesModule { }
