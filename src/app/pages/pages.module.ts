75a03fb4eb68c7fbb5cb08bf1f02e143b0d24e64
Gilberto Jr <giljrsantos@gmail.com>
2023-02-18 20:48:40 -0300

    Importando o modulo Share

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
+import { DetailsComponent } from './details/details.component';
 // Pages
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 // Modele Routing
 import { RoutingModule } from './routing-module';
-import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
