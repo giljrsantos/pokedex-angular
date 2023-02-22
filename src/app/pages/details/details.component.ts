067682614383e059db18b057ec28974bc0e1babe
Gilberto Jr <giljrsantos@gmail.com>
2023-02-21 21:04:31 -0300

    Fazendo o filtro de um determinado pokemon e também criando pagina de detalhes do pokemon

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+import { PokeApiService } from 'src/app/service/poke-api.service';
+import { forkJoin } from 'rxjs';
+
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  //URL
+  private urlPokemon: string = `https://pokeapi.co/api/v2/pokemon`;
+  private urlName: string = `https://pokeapi.co/api/v2/pokemon-species`;
+
+  public pokemon: any;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) { }
 
   ngOnInit(): void {
+    this.getPokemon;
+  }
+
+  get getPokemon(){
+
+    const id = this.activatedRoute.snapshot.params['id'];
+
+    const pokemon = this.pokeApiService.apiGetPokemon(`${this.urlPokemon}/${id}`);
+
+    const name = this.pokeApiService.apiGetPokemon(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(res => {
+      
+      this.pokemon = res;
+      
+    });
+
+   
   }
 
 }
